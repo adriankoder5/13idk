751b06a711b630535d7efab69605fa5403871a62
adriankoder5 <adriankoder5@gmail.com>
2024-11-11 13:13:39 +0100

    bugfixcalcShowVar

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
-console.log('Hello!');
+console.log('testekko')
+
+function persInf(){
+    let usrN ='adr';
+    let usrAge =19;
+    let isOnline =true;
+    console.log(usrN,typeof(usrN),usrAge,typeof(usrAge),isOnline,typeof(isOnline));
+}
+//persInf();
+
+function simpleCalc(num1,num2){
+    let summer = num1+num2;
+    console.log(num1,'+',num2,'=',summer,typeof(summer),'func = simpleCalc');
+    return(summer);
+}
+simpleCalc(2,4)
+console.log('returnTest',simpleCalc(6,4));
+
+const persShowVar = document.querySelector("#persShow");
+console.log(persShowVar);
+persShowVar.addEventListener("click", persInf);
+
+const calcShowVar = document.querySelector("#calcShow");
+console.log(calcShowVar);
+//calcShowVar.addEventListener("click", simpleCalc(3, 4));
+
+calcShowVar.addEventListener("click", function (){simpleCalc(3,4)});
